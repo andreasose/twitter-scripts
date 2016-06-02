ef566d7b09a9e6418a0e983c8d0dca5e016262e8
andreasose <andreasose@outlook.com>
2016-06-02 11:31:36 +0200

    updated functions

--- tdmCreator.R
+++ tdmCreator.R
@@ -1,14 +1,15 @@
-tdmCreator <- function(dataframe, term.freq = 10, stemDoc = F ){
-    library(tm)
-
-corpus <- Corpus(VectorSource(dataframe$text))
-corpus <- tm_map(corpus, removeWords, stopwords())
-if (isTRUE(stemDoc)) {
-    corpus <- tm_map(corpus, stemDocument)
-}
-tdm <- TermDocumentMatrix(corpus, control = list(wordLengths = c(1, Inf)))
-term.freq <- rowSums(as.matrix(tdm))
-term.freq <- subset(term.freq, term.freq >= term.freq)
-df <- data.frame(term = names(term.freq), freq = term.freq)
-return(df)
+tdmCreator <- function(dataframe, term.freq = 10, stemDoc = F, rmStopwords = T){
+    
+    corpus <- Corpus(VectorSource(dataframe$text))
+    if (isTRUE(rmStopwords)) {
+        corpus <- tm_map(corpus, removeWords, stopwords())
+    }
+    if (isTRUE(stemDoc)) {
+        corpus <- tm_map(corpus, stemDocument)
+    }
+    tdm <- TermDocumentMatrix(corpus, control = list(wordLengths = c(1, Inf)))
+    termFreq <- rowSums(as.matrix(tdm))
+    termFreq <- subset(termFreq, termFreq >= term.freq)
+    df <- data.frame(term = names(termFreq), freq = termFreq)
+    return(df)
 }
--- textScrubber.R
+++ textScrubber.R
@@ -1,8 +1,12 @@
 textScrubber <- function(dataframe) {
-    dataframe$text <- gsub("[[:punct:]]", "", dataframe$text)
+    
+    dataframe$text <-  gsub("[[:punct:]]", "", dataframe$text)
     dataframe$text <-  gsub("[[:digit:]]", "", dataframe$text)
     dataframe$text <-  gsub("http\\w+", "", dataframe$text)
+    dataframe$text <-  gsub("\n", " ", dataframe$text)
     dataframe$text <-  gsub("[ \t]{2,}", "", dataframe$text)
     dataframe$text <-  gsub("^\\s+|\\s+$", "", dataframe$text)
+    dataframe$text <-  tolower(dataframe$text)
+    
     return(dataframe)
 }
