e16406b61cb261b77edc72cfc77651bca260fbb8
andreasose <andreasose@outlook.com>
2016-06-16 19:37:03 +0200

    updated with kernel density estimates of tweeting time

--- tdmCreator.R
+++ tdmCreator.R
@@ -8,7 +8,6 @@ tdmCreator <- function(dataframe, stemDoc = T, rmStopwords = T){
         corpus <- tm_map(corpus, stemDocument)
     }
     tdm <- TermDocumentMatrix(corpus, control = list(wordLengths = c(1, Inf)))
-    tdm <- removeSparseTerms(tdm, sparse= 0.97)
     termFreq <- rowSums(as.matrix(tdm))
     termFreq <- sort(termFreq, decreasing = T)
     df <- data.frame(term = names(termFreq), freq = termFreq)
